1bdb94262c04f11f48422126ae2f17e51b914d1e
Jacky Chen <chenjacky32@gmail.com>
2024-01-09 19:48:14 +0700

    feat:tambah fitur search dan shopping cart

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-// Toggle Class active
+// Toggle Class active utk hamburger menu
 const navbarNav = document.querySelector(".navbar-nav");
 
 //Ketika Hamburger diklik
@@ -6,11 +6,36 @@ document.querySelector("#hamburger-menu").onclick = () => {
   navbarNav.classList.toggle("active");
 };
 
-//klik diluar sidebar untuk menghilangkan nav
-const Hamburger = document.querySelector("#hamburger-menu");
+//klik diluar elemen
+const Hm = document.querySelector("#hamburger-menu");
+const Sb = document.querySelector("#search-button");
+const Scb = document.querySelector("#shopping-cart-button");
 
 document.addEventListener("click", function (e) {
-  if (!Hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+  if (!Hm.contains(e.target) && !navbarNav.contains(e.target)) {
     navbarNav.classList.remove("active");
   }
+  if (!Sb.contains(e.target) && !searchForm.contains(e.target)) {
+    searchForm.classList.remove("active");
+  }
+  if (!Scb.contains(e.target) && !shoppingcart.contains(e.target)) {
+    shoppingcart.classList.remove("active");
+  }
 });
+
+// Toggle Class active utk search form
+const searchForm = document.querySelector(".search-form");
+const searchBox = document.querySelector("#search-box");
+
+document.querySelector("#search-button").onclick = (e) => {
+  searchForm.classList.toggle("active");
+  searchBox.focus();
+  e.preventDefault();
+};
+
+//Toggle Class active utk shopping cart
+const shoppingcart = document.querySelector(".shopping-cart");
+document.querySelector("#shopping-cart-button").onclick = (e) => {
+  shoppingcart.classList.toggle("active");
+  e.preventDefault();
+};
